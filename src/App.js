9c69e58b99b0b271fc361a5c20c0e2749bd7e613
gracekim527 <k07173027@gmail.com>
2025-10-23 16:14:55 +0900

    :memo: Docs(readme): 구현할 기능 및 예외처리 작성
    구현할 기능 목록, 기능, 예외처리, 프로그래밍 요구사항을 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const number = MissionUtils.Random.pickNumberInRange(0, 9);
+    console.log(number);
+  }
 }
 
 export default App;
